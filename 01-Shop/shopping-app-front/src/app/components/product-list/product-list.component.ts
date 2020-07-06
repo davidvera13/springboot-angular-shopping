f6910c49a63a275e16455a3b66e95e0aa8e9b0af
David Vera <david.vera@9online.fr>
2020-07-06 08:02:41 +0200

    feat: Display Category Name in Product List Grid

--- 01-Shop/shopping-app-front/src/app/components/product-list/product-list.component.ts
+++ 01-Shop/shopping-app-front/src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number;
+  currentCategoryName: string;
   searchMode: boolean;
 
   constructor(private productService: ProductService, private route: ActivatedRoute) { }
@@ -37,11 +38,15 @@ export class ProductListComponent implements OnInit {
     // check if id parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
     if (hasCategoryId) {
-      // get the id and convert to number
+      // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+
+      // get the "name" param string
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name');
     } else {
       // id is set to 1 (default value)
       this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
     this.productService.getCategoryProductList(this.currentCategoryId).subscribe(
       data => { this.products = data }
